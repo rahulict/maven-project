d968e58d8c796fdd3e7a30fd45309af4b431d9da
Rahul Kumar <rahul.kumar808@gmail.com>
2018-01-31 19:33:39 +0530

    Correction

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,9 @@ public class Greeter {
 
   }
 
+  /**
+   * This is a constructor.
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
